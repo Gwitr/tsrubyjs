5b83544b853b9407a3cc4b115427946be15ba908
Robowiko123 <robowiko@gmail.com>
2020-12-21 21:20:40 +0100

    restore old example code

--- ast.js
+++ ast.js
@@ -4,92 +4,148 @@ export const ast = new RubyNode('begin', [
     new RubyNode('class', [
         new RubyNode('const', [
             null,
-            'A'
+            'Range'
         ]),
         null,
-        new RubyNode('def', [
-            'f',
-            new RubyNode('args'),
-            new RubyNode('send', [
-                null,
-                'puts',
-                new RubyNode('str', ['f'])
-            ])
-        ])
-    ]),
-    new RubyNode('class', [
-        new RubyNode('const', [
-            null,
-            'B'
-        ]),
-        new RubyNode('const', [
-            null,
-            'A'
-        ]),
-        new RubyNode('def', [
-            'f3',
-            new RubyNode('args'),
-            new RubyNode('send', [
-                null,
-                'puts',
-                new RubyNode('str', ['f3'])
-            ])
-        ])
-    ]),
-    new RubyNode('class', [
-        new RubyNode('const', [
-            null,
-            'A'
-        ]),
-        null,
-        new RubyNode('def', [
-            'f2',
-            new RubyNode('args'),
-            new RubyNode('send', [
-                null,
-                'puts',
-                new RubyNode('str', ['f2'])
+        new RubyNode('begin', [
+            new RubyNode('def', [
+                'initialize',
+                new RubyNode('args', [
+                    new RubyNode('arg', ['start']),
+                    new RubyNode('arg', ['r_end']),
+                    new RubyNode('optarg', [
+                        'step',
+                        new RubyNode('int', [1])
+                    ])
+                ]),
+                new RubyNode('begin', [
+                    new RubyNode('ivasgn', [
+                        '@start',
+                        new RubyNode('lvar', ['start'])
+                    ]),
+                    new RubyNode('ivasgn', [
+                        '@r_end',
+                        new RubyNode('lvar', ['r_end'])
+                    ]),
+                    new RubyNode('ivasgn', [
+                        '@step',
+                        new RubyNode('lvar', ['step'])
+                    ])
+                ])
+            ]),
+            new RubyNode('def', [
+                '[]',
+                new RubyNode('args', [new RubyNode('arg', ['i'])]),
+                new RubyNode('begin', [
+                    new RubyNode('lvasgn', [
+                        'x',
+                        new RubyNode('send', [
+                            new RubyNode('ivar', ['@start']),
+                            '+',
+                            new RubyNode('send', [
+                                new RubyNode('lvar', ['i']),
+                                '*',
+                                new RubyNode('ivar', ['@step'])
+                            ])
+                        ])
+                    ]),
+                    new RubyNode('if', [
+                        new RubyNode('send', [
+                            new RubyNode('lvar', ['x']),
+                            '<',
+                            new RubyNode('ivar', ['@r_end'])
+                        ]),
+                        new RubyNode('return', [new RubyNode('lvar', ['x'])]),
+                        null
+                    ]),
+                    new RubyNode('send', [
+                        null,
+                        'raise',
+                        new RubyNode('send', [
+                            new RubyNode('const', [
+                                null,
+                                'IndexError'
+                            ]),
+                            'new',
+                            new RubyNode('str', ['Index out of range'])
+                        ])
+                    ])
+                ])
+            ]),
+            new RubyNode('def', [
+                'each',
+                new RubyNode('args', [new RubyNode('blockarg', ['b'])]),
+                new RubyNode('begin', [
+                    new RubyNode('lvasgn', [
+                        'x',
+                        new RubyNode('ivar', ['@start'])
+                    ]),
+                    new RubyNode('while', [
+                        new RubyNode('send', [
+                            new RubyNode('lvar', ['x']),
+                            '<',
+                            new RubyNode('ivar', ['@r_end'])
+                        ]),
+                        new RubyNode('begin', [
+                            new RubyNode('send', [
+                                new RubyNode('lvar', ['b']),
+                                'call',
+                                new RubyNode('lvar', ['x'])
+                            ]),
+                            new RubyNode('op_asgn', [
+                                new RubyNode('lvasgn', ['x']),
+                                '+',
+                                new RubyNode('ivar', ['@step'])
+                            ])
+                        ])
+                    ])
+                ])
             ])
         ])
     ]),
     new RubyNode('lvasgn', [
-        'x',
+        'r',
         new RubyNode('send', [
             new RubyNode('const', [
                 null,
-                'A'
+                'Range'
             ]),
-            'new'
+            'new',
+            new RubyNode('int', [-100]),
+            new RubyNode('int', [100]),
+            new RubyNode('int', [3])
         ])
     ]),
-    new RubyNode('send', [
-        new RubyNode('lvar', ['x']),
-        'f'
-    ]),
-    new RubyNode('send', [
-        new RubyNode('lvar', ['x']),
-        'f2'
-    ]),
-    new RubyNode('lvasgn', [
-        'x',
+    new RubyNode('block', [
         new RubyNode('send', [
-            new RubyNode('const', [
-                null,
-                'B'
+            new RubyNode('lvar', ['r']),
+            'each'
+        ]),
+        new RubyNode('args', [new RubyNode('procarg0', [new RubyNode('arg', ['n'])])]),
+        new RubyNode('begin', [
+            new RubyNode('if', [
+                new RubyNode('send', [
+                    new RubyNode('lvar', ['n']),
+                    '<',
+                    new RubyNode('int', [-40])
+                ]),
+                new RubyNode('next'),
+                null
             ]),
-            'new'
+            new RubyNode('if', [
+                new RubyNode('send', [
+                    new RubyNode('lvar', ['n']),
+                    '>',
+                    new RubyNode('int', [40])
+                ]),
+                new RubyNode('next'),
+                null
+            ]),
+            new RubyNode('send', [
+                null,
+                'puts',
+                new RubyNode('lvar', ['n'])
+            ])
         ])
-    ]),
-    new RubyNode('send', [
-        new RubyNode('lvar', ['x']),
-        'f'
-    ]),
-    new RubyNode('send', [
-        new RubyNode('lvar', ['x']),
-        'f3'
-    ]),
-    new RubyNode('send', [
-        new RubyNode('lvar', ['x']),
-        'f2'
     ])
 ]);
